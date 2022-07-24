7332250b724fb2826c3164f9890aa44bb14c5ed6
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2022-07-24 17:05:29 +0300

    add a click event to button

--- app/src/main/java/com/piy/sevenminutesworkout/MainActivity.kt
+++ app/src/main/java/com/piy/sevenminutesworkout/MainActivity.kt
@@ -2,10 +2,20 @@ package com.piy.sevenminutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        start_button.setOnClickListener {
+            Toast.makeText(
+                this,
+                "Start an exercise", Toast.LENGTH_LONG
+            ).show()
+        }
     }
 }
